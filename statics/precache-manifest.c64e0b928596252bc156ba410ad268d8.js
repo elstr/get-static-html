f5149fddc54be87d36b74acd713dea75525edb81
Eleonora Lester <lele@Eleonoras-MacBook-Pro.local>
2020-10-02 16:40:18 -0300

    statics

---
+++ statics/precache-manifest.c64e0b928596252bc156ba410ad268d8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac96edb8de049e991f21a49853cc3f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "494c2cf16f27e1ebc65f",
+    "url": "/static/css/main.8d37e5d1.chunk.css"
+  },
+  {
+    "revision": "56046cf26c8f7ae3ac8d",
+    "url": "/static/js/2.8b66eb7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8b66eb7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "494c2cf16f27e1ebc65f",
+    "url": "/static/js/main.a718596f.chunk.js"
+  },
+  {
+    "revision": "054edf690612692b45f8",
+    "url": "/static/js/runtime-main.86945ae8.js"
+  },
+  {
+    "revision": "ae38dd1096b223387beca7e9063e7ea1",
+    "url": "/static/media/arrow-up.ae38dd10.svg"
+  },
+  {
+    "revision": "7ae639b11db4d99fb07bbc4256aff4f4",
+    "url": "/static/media/chevron-next.7ae639b1.svg"
+  },
+  {
+    "revision": "0955d933f44adcbc2632e32e15e4fdc2",
+    "url": "/static/media/chevron-prev.0955d933.svg"
+  },
+  {
+    "revision": "efbc157978b08a50522cfdae0fdc85b8",
+    "url": "/static/media/close.efbc1579.svg"
+  },
+  {
+    "revision": "d2c1fdbc48bd79fdcbce11e9ae54b2b9",
+    "url": "/static/media/logo.d2c1fdbc.svg"
+  },
+  {
+    "revision": "0b6413d063d5b8f4f738b7a8a56351f7",
+    "url": "/static/media/menu.0b6413d0.svg"
+  },
+  {
+    "revision": "df863950934e8150ae8afd69a3b1b69d",
+    "url": "/static/media/parachute-cat.df863950.svg"
+  }
+]);
